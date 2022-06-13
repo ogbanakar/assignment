435fd8070382aa87e992c58a1b0837762c88a636
ogbanakar <86116916+ogbanakar@users.noreply.github.com>
2022-06-13 10:32:37 +0530

    master

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Axios from 'axios'
 
 function App() {
-  return (
-    z
+ const getUser = () => {
+    Axios.get('https://reqres.in/api/users?page=2').then((response) => {
+      console.log(response);
+    }
+    );
+ };
+  return( 
+    <div>
+    <button onClick={getUser}> Get User</button>
+    </div>
   );
 }
 
